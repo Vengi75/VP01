6eeb6543c2b90e4577a354a6d064b20a824b5235
Vengi57 <Vengi57@meggido.co.za>
2018-11-09 07:02:49 +0200

    initial prj commit

---
+++ src/MTG_Deck_Sorter.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : MTG_Deck_Sorter.cpp
+// Author      : Vengi
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "MTG_Deck_Sorter" << endl; // prints MTG_Deck_Sorter
+	return 0;
+}
